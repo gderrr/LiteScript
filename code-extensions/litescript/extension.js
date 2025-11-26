6c23d675b5513bf5b3609bfabea2b8846f3ffecb
hadeer.abbas.khalil <hadeer.abbas.khalil@estudiantat.upc.edu>
2025-11-26 20:03:18 +0100

    First prototype of LTS VSCode Extension, recognizes .lts files as LiteScript

---
+++ code-extensions/litescript/extension.js
@@ -0,0 +1,9 @@
+const vscode = require('vscode');
+
+function activate(context) {
+    console.log('LiteScript activated.');
+}
+
+function deactivate() {}
+
+module.exports = { activate, deactivate };
