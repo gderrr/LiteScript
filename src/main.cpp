63396ecf27903def182a310d3dfd1f4e2340bb85
hadeer.abbas.khalil <hadeer.abbas.khalil@estudiantat.upc.edu>
2026-02-16 00:34:19 +0100

    small fix

--- src/main.cpp
+++ src/main.cpp
@@ -84,8 +84,8 @@ vector<string> split (int& tabCount, const string& line) {
 
 void injectUnderscores (string& line) {
     bool inString = false;
-    for (int i = 0; i < line.size(); i++) {
-        if (line[i] == '\"') inString = !inString;
+    for (int i = 1; i < line.size(); i++) {
+        if (line[i] == '\"' && line[i-1] != "\\") inString = !inString;
         else if (line[i] == ' ' && inString) line[i] = '_';
         else if (line[i] == '_' && inString) line.insert(i++, "\\");
     }
