eafc6d91c5833b21113db7eb5572d6df3b69e4e7
hadeer.abbas.khalil <hadeer.abbas.khalil@estudiantat.upc.edu>
2026-02-26 23:00:12 +0100

    bump, finished Introduction

--- docs/litescript-docs/src/pages/index.js
+++ docs/litescript-docs/src/pages/index.js
@@ -25,15 +25,15 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Reference`}
+      description="">
       <HomepageHeader />
       <main>
         <div id="frame">
           <img id="mainlogo" src="/img/logo.svg"/>
         </div>
         <div id="desc">
-          <p>
+        <p>
           LiteScript is a dynamic procedural fixed-form line based interpreted toy programming 
           language initially built just for turing completeness that has expanded to provide a 
           wide variety of functionalities suitable for executing small and simple scripts and 
@@ -41,7 +41,7 @@ export default function Home() {
         </p>
         <p>
           Its syntax is based off a high level dynamic scripting language like Python while
-          adhering to the philosophy and design principals of lower level language like assembly
+          adhering to the philosophy and design principals of lower level languages like assembly
           and the C programming language, to create a scripting paradigm that sticks to a 
           style closely resembling to how a computer actually runs under the hood.
         </p>
@@ -56,9 +56,54 @@ export default function Home() {
             <li>A very simple GUI library to create simple windows and forms.</li>
             <li>Database connections and interactions with transactions via SQLite and PostgreSQL.</li>
           </ol>
-          Combining the functionality of the different integrated modules we extend LiteScript from
-          just turing completeness to allow it to even set up simple server-side applications.
+          Combining the functionality of the different integrated modules we extend LiteScript 
+          from just turing completeness to allow it to even set up simple server-side applications.
         </p>
+        <p>
+          In the following reference we try to reference all the relevant core functionalities and 
+          utilities that the language provides, so that you, the programmer, can utilize them to 
+          their fullest extent in your scripts.
+        </p>
+
+        <h1>Philosophy</h1>
+        <p>
+          Since LiteScript is an open source project, we want to make the language and its runtime
+          accessible to everybody, taking a firm stance against corporate proprietary software,
+          therefore, the interpreter/runtime is designed to run <strong>exclusively on the Linux kernel</strong>,
+          allowing LiteScript to be executed in any Linux distribution (Ubuntu, Arch, Kali, Mint, etc.).
+        </p>
+        <p>
+          Despite LiteScript's primary purpose of being a dynamic scripting language it borrows the
+          philosophy and design principals of assembly, therefore its style reflects closely to how a
+          computer executes programs, while only providing the abstractions we deem necessary for dynamic
+          scripting. By adhering to this philosophy, LiteScript will only support imperative
+          and procedural programming, since at a low level this is what gets compiled/assembled to, and
+          will <strong>never support Object-Oriented Programming</strong>. Everything that requires the program to handle
+          mutable states and custom objects will be handled implicitly by the interpreter's runtime.
+        </p> 
+        <h1>Runtime</h1>
+        <p>
+          LiteScript is executed with its own interpreter, called 'Lite'. To install it, just follow
+          the installation steps found in the <a href="https://github.com/gderrr/LiteScript">LiteScript repository</a>.
+        </p>
+        <p>
+          Once installed correctly into the binaries of your Linux machine, just run it with the following command:
+        </p>
+        <pre>
+            <code>lite [FILENAME] [ARGS...]</code>
+        </pre>
+        <p>
+          Where the FILENAME is a file ending with the .lts extension containing your LiteScript program,
+          and ARGS... are any additional arguments you want to run with your program, just like a C or C++ program.
+        </p>
+        <p>
+          You can also check the version and release date of your LiteScript interpreter by running lite with 
+          the following flag:
+        </p>
+        <pre>
+          <code>lite --version</code>
+        </pre>
+
         </div>
       </main>
     </Layout>
